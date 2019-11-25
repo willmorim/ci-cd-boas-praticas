af5b6de89d8bab0039cefc91b2dcffe02b5b401d
willmorim <pdbawilliam@gmail.com>
2019-11-25 18:17:45 -0300

    fix eslint

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.get('/', (res, res )=> {
-  res.send('ok');
+app.get('/', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(3000, () => console.log('RUNNING ON 3000'))
